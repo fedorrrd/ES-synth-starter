9b28c394b0110e28bb667ea203fa6718e5ea841e
kr1pt0 <fedorrrd@gmail.com>
2025-03-05 10:33:39 +0000

    creating sound

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,147 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <bitset>
 
-//Constants
-  const uint32_t interval = 100; //Display update interval
+HardwareTimer sampleTimer(TIM1);
+std::bitset<32> inputs;
 
-//Pin definitions
-  //Row select and enable
-  const int RA0_PIN = D3;
-  const int RA1_PIN = D6;
-  const int RA2_PIN = D12;
-  const int REN_PIN = A5;
+// Constants
+const uint32_t interval = 100;                                                                                                                         // Display update interval
+const uint32_t frequencies[] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 444, 466, 494};                                                           // frequencies of notes for sawtooth
+const uint32_t stepSizes[] = {51149156, 54077543, 57396381, 60715219, 64424509, 68133799, 72233541, 76528508, 81018701, 86680249, 90975216, 96441538}; // stepsizes for sawtooth
+const std::string notes[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
-  //Matrix input and output
-  const int C0_PIN = A2;
-  const int C1_PIN = D9;
-  const int C2_PIN = A6;
-  const int C3_PIN = D1;
-  const int OUT_PIN = D11;
+// volatiles
+volatile uint32_t currentStepSize;
+volatile uint32_t localCurrentStepSize;
 
-  //Audio analogue out
-  const int OUTL_PIN = A4;
-  const int OUTR_PIN = A3;
+//struct to store variables between threads
+struct {
+  std::bitset<32> inputs;  
+  } sysState;
 
-  //Joystick analogue in
-  const int JOYY_PIN = A0;
-  const int JOYX_PIN = A1;
+// Pin definitions
+// Row select and enable
+const int RA0_PIN = D3;
+const int RA1_PIN = D6;
+const int RA2_PIN = D12;
+const int REN_PIN = A5;
 
-  //Output multiplexer bits
-  const int DEN_BIT = 3;
-  const int DRST_BIT = 4;
-  const int HKOW_BIT = 5;
-  const int HKOE_BIT = 6;
+// Matrix input and output
+const int C0_PIN = A2;
+const int C1_PIN = D9;
+const int C2_PIN = A6;
+const int C3_PIN = D1;
+const int OUT_PIN = D11;
 
-//Display driver object
+// Audio analogue out
+const int OUTL_PIN = A4;
+const int OUTR_PIN = A3;
+
+// Joystick analogue in
+const int JOYY_PIN = A0;
+const int JOYX_PIN = A1;
+
+// Output multiplexer bits
+const int DEN_BIT = 3;
+const int DRST_BIT = 4;
+const int HKOW_BIT = 5;
+const int HKOE_BIT = 6;
+
+// Display driver object
 U8G2_SSD1305_128X32_ADAFRUIT_F_HW_I2C u8g2(U8G2_R0);
 
-//Function to set outputs using key matrix
-void setOutMuxBit(const uint8_t bitIdx, const bool value) {
-      digitalWrite(REN_PIN,LOW);
-      digitalWrite(RA0_PIN, bitIdx & 0x01);
-      digitalWrite(RA1_PIN, bitIdx & 0x02);
-      digitalWrite(RA2_PIN, bitIdx & 0x04);
-      digitalWrite(OUT_PIN,value);
-      digitalWrite(REN_PIN,HIGH);
-      delayMicroseconds(2);
-      digitalWrite(REN_PIN,LOW);
+// read inputs
+std::bitset<4> readCols()
+{
+  std::bitset<4> result;
+  result[0] = digitalRead(C0_PIN);
+  result[1] = digitalRead(C1_PIN);
+  result[2] = digitalRead(C2_PIN);
+  result[3] = digitalRead(C3_PIN);
+
+  return result;
 }
 
-void setup() {
+void setRow(uint8_t rowIdx)
+{
+  digitalWrite(REN_PIN, LOW);
+
+  digitalWrite(RA0_PIN, rowIdx & 1);
+  digitalWrite(RA1_PIN, rowIdx & 2);
+  digitalWrite(RA2_PIN, rowIdx & 4);
+
+  digitalWrite(REN_PIN, HIGH);
+}
+
+void sampleISR()
+{
+  static uint32_t phaseAcc = 0;
+  phaseAcc += currentStepSize;
+  int32_t Vout = (phaseAcc >> 24) - 128;
+  analogWrite(OUTR_PIN, Vout + 128);
+}
+
+// Function to set outputs using key matrix
+void setOutMuxBit(const uint8_t bitIdx, const bool value)
+{
+  digitalWrite(REN_PIN, LOW);
+  digitalWrite(RA0_PIN, bitIdx & 0x01);
+  digitalWrite(RA1_PIN, bitIdx & 0x02);
+  digitalWrite(RA2_PIN, bitIdx & 0x04);
+  digitalWrite(OUT_PIN, value);
+  digitalWrite(REN_PIN, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(REN_PIN, LOW);
+}
+
+void scanKeysTask(void * pvParameters) {
+  for (int i = 0; i < 3; i++)
+  {
+    setRow(i);
+    delayMicroseconds(3);
+    std::bitset<4> input_iteration = readCols();
+    for (int j = 0; j < 4; j++)
+    {
+      sysState.inputs[i * 4 + j] = input_iteration[j];
+    }
+  }
+
+  bool keypress = false;
+
+  for (int i = 0; i < 12; i++)
+  {
+    if (sysState.inputs[i] == 0)
+    {
+      localCurrentStepSize = stepSizes[i];
+      u8g2.clearBuffer();
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.drawStr(2, 10, notes[i].c_str());
+      u8g2.sendBuffer();
+      keypress = true;
+    }
+  }
+
+  if (!keypress)
+  {
+    localCurrentStepSize = 0;
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.drawStr(2, 10, "no keypress");
+    u8g2.sendBuffer();
+  }
+
+  __atomic_store_n(&currentStepSize, localCurrentStepSize, __ATOMIC_RELAXED);
+
+  // Toggle LED
+  digitalToggle(LED_BUILTIN);
+}
+
+void setup()
+{
   // put your setup code here, to run once:
 
-  //Set pin directions
+  // Set pin directions
   pinMode(RA0_PIN, OUTPUT);
   pinMode(RA1_PIN, OUTPUT);
   pinMode(RA2_PIN, OUTPUT);
@@ -67,36 +158,33 @@ void setup() {
   pinMode(JOYX_PIN, INPUT);
   pinMode(JOYY_PIN, INPUT);
 
-  //Initialise display
-  setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
+  // Initialise display
+  setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
   delayMicroseconds(2);
-  setOutMuxBit(DRST_BIT, HIGH);  //Release display logic reset
+  setOutMuxBit(DRST_BIT, HIGH); // Release display logic reset
   u8g2.begin();
-  setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
+  setOutMuxBit(DEN_BIT, HIGH); // Enable display power supply
 
-  //Initialise UART
+  // Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
+
+  // setup interrupt
+  sampleTimer.setOverflow(22000, HERTZ_FORMAT);
+  sampleTimer.attachInterrupt(sampleISR);
+  sampleTimer.resume();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   static uint32_t next = millis();
   static uint32_t count = 0;
 
-  while (millis() < next);  //Wait for next interval
+  while (millis() < next)
+    ; // Wait for next interval
 
   next += interval;
 
-  //Update display
-  u8g2.clearBuffer();         // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
-  u8g2.setCursor(2,20);
-  u8g2.print(count++);
-  u8g2.sendBuffer();          // transfer internal memory to the display
-
-  //Toggle LED
-  digitalToggle(LED_BUILTIN);
-  
+  scanKeysTask(NULL);
 }
